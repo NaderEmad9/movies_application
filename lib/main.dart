b01dd5a718bff38af326f77601742c2f2b68f98b
SaraElnagar6 <SaraElnagar@gmail.com>
2024-08-22 05:35:31 +0300

    Create Design Movie Details Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:movies_application/app_screen/home_screen.dart';
+import 'package:movies_application/home_tab/movie_details/movie_details_screen.dart';
+// import 'package:movies_application/app_screen/home_screen.dart';
 import 'package:movies_application/ui/app_theme_data.dart';
+
+import 'app_screen/app_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,6 +21,7 @@ class MyApp extends StatelessWidget {
       initialRoute: AppScreen.routeName,
       routes: {
         AppScreen.routeName: (context) => const AppScreen(),
+        MovieDetailsScreen.routeName: (context) => MovieDetailsScreen(),
       },
     );
   }
