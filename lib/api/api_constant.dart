93d2e844d894a419a1f8a42abf04fc54e54feea7
RofaidaAhmed-100 <rofaidaahmed879@gmail.com>
2024-08-30 16:46:55 +0300

    finished topRated api in home screen and added class movie

--- lib/api/api_constant.dart
+++ lib/api/api_constant.dart
@@ -2,7 +2,7 @@ class ApiConstant {
   ///https://api.themoviedb.org/3/movie/popular
   static const String scheme = "https";
   static const String host = 'api.themoviedb.org';
-  static const String popularMoviespath = '/3/movie/popular';
+ static const String popularMoviespath = '/3/movie/popular';
   static const String token =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTY2YjQ0Yjc0ZDlkZjA3ZDBjYjRmYjNhNzAyZDk0ZiIsIm5iZiI6MTcyNDY5MzQ2MS4wMzcyNzQsInN1YiI6IjY2YzNhMWUwODlhMTI5ZGJmNzZlM2NmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K_cli2oWxsRjYaiRYclJhflNgdDtbsPLLoNeHYsKigk';
   static const String newReleasesMoviespath = '/3/movie/upcoming';
--- lib/home_tab/home_tab.dart
+++ lib/home_tab/home_tab.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 import 'package:movies_application/api/api_manager.dart';
 import 'package:movies_application/home_tab/Content_recommended.dart';
 import 'package:movies_application/home_tab/content_newrealease.dart';
@@ -8,6 +9,7 @@ import 'package:movies_application/home_tab/movie_details/screen/movie_details_s
 import '../model/PopularResponse.dart';
 import '../ui/app_colors.dart';
 import '../model/NewReleaseResponse.dart';
+import '../model/TopRatedResponse.dart';
 
 class HomeTab extends StatefulWidget {
   const HomeTab({super.key});
@@ -40,226 +42,268 @@ class HomeTabState extends State<HomeTab> {
     var releases = AppLocalizations.of(context)!.new_release;
 
     return Scaffold(
-        backgroundColor: AppColors.blackColor,
-        body: ListView(
+      backgroundColor: AppColors.blackColor,
+      body: ListView(
         children: [
-        FutureBuilder<HttpResponse<PopularResponse>>(
-        future: ApiManager.getPopularMovies(),
-    builder: (context, snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-    return const Center(
-    child: CircularProgressIndicator(
-    color: AppColors.whiteColor,
-    ),
-    );
-    } else if (snapshot.hasError || snapshot.data?.statusCode != 200) {
-    return Column(
-    children: [
-    const Text('Something went wrong'),
-    ElevatedButton(
-    onPressed: () {
-    setState(() {});
-    },
-    child: const Text('Try again'),
-    ),
-    ],
-    );
-    } else if (snapshot.hasData &&
-    snapshot.data?.data.results != null &&
-    snapshot.data!.data.results!.isNotEmpty) {
-    var movies = snapshot.data!.data.results!;
+          FutureBuilder<HttpResponse<PopularResponse>>(
+            future: ApiManager.getPopularMovies(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: AppColors.whiteColor,
+                  ),
+                );
+              } else if (snapshot.hasError || snapshot.data?.statusCode != 200) {
+                return Column(
+                  children: [
+                    const Text('Something went wrong'),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: const Text('Try again'),
+                    ),
+                  ],
+                );
+              } else if (snapshot.hasData &&
+                  snapshot.data?.data.results != null &&
+                  snapshot.data!.data.results!.isNotEmpty) {
+                var movies = snapshot.data!.data.results!;
 
-    return Column(
-    children: [
-    CarouselSlider.builder(
-    itemCount: movies.length,
-    itemBuilder: (context, index, realIndex) {
-    var movie = movies[index];
-    var imageUrl = 'https://image.tmdb.org/t/p/w500${movie.posterPath}';
+                return Column(
+                  children: [
+                    CarouselSlider.builder(
+                      itemCount: movies.length,
+                      itemBuilder: (context, index, realIndex) {
+                        var movie = movies[index];
+                        var imageUrl = 'https://image.tmdb.org/t/p/w500${movie.posterPath}';
 
-    return InkWell(
-    onTap: () {
-    Navigator.pushNamed(context, MovieDetailsScreen.routeName);
-    },
-    child: Stack(
-    clipBehavior: Clip.none,
-    children: [
-    // Background image
-    Positioned.fill(
-    child: Image.network(
-    imageUrl,
-    fit: BoxFit.fill,
-    ),
-    ),
+                        return InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, MovieDetailsScreen.routeName);
+                          },
+                          child: Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              Positioned.fill(
+                                child: Image.network(
+                                  imageUrl,
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                              Center(
+                                child: Icon(
+                                  Icons.play_circle_filled,
+                                  size: 64,
+                                  color: AppColors.whiteColor,
+                                ),
+                              ),
+                              Positioned(
+                                left: width * 0.05,
+                                bottom: -height * 0.1,
+                                child: Container(
+                                  width: width * 0.32,
+                                  height: height * 0.225,
+                                  decoration: BoxDecoration(
+                                    color: AppColors.lightGreyColor,
+                                    borderRadius: BorderRadius.circular(6),
+                                  ),
+                                  child: Stack(
+                                    children: [
+                                      Center(
+                                        child: InkWell(
+                                          onTap: () {
+                                            Navigator.pushNamed(
+                                              context,
+                                              MovieDetailsScreen.routeName,
+                                              arguments: movie,
+                                            );
+                                          },
+                                          child: Image.network(
+                                            imageUrl,
+                                            fit: BoxFit.fill,
+                                            width: double.infinity,
+                                            height: double.infinity,
+                                          ),
+                                        ),
+                                      ),
+                                      Align(
+                                        alignment: Alignment.topLeft,
+                                        child: IconButton(
+                                          icon: _isTopContainerBookmarked
+                                              ? const Icon(
+                                            Icons.bookmark_added,
+                                            color: AppColors.orangeColor,
+                                          )
+                                              : const Icon(
+                                            Icons.bookmark_add_outlined,
+                                            color: AppColors.whiteColor,
+                                          ),
+                                          onPressed: _handleTopContainerBookmarkChange,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: width * 0.4,
+                                bottom: height * 0.05,
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: width * 0.02),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        movie.title ?? "Title not available",
+                                        style: Theme.of(context).textTheme.headlineLarge,
+                                      ),
+                                      SizedBox(height: height * 0.005),
+                                      Text(
+                                        movie.releaseDate ?? "Date not available",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .labelLarge!
+                                            .copyWith(
+                                          color: AppColors.moviesDetailsColor.withOpacity(0.7),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      options: CarouselOptions(
+                        height: height * 0.35,
+                        autoPlay: true,
+                        aspectRatio: 16 / 9,
+                        viewportFraction: 0.8,
+                      ),
+                    ),
+                    SizedBox(height: height * 0.015),
 
-    Center(
-    child: Icon(
-    Icons.play_circle_filled,
-    size: 64,
-    color: AppColors.whiteColor,
-    ),
-    ),
+                    // FutureBuilder for new releases
+                    FutureBuilder<HttpResponse<NewReleaseResponse>>(
+                      future: ApiManager.getNewReleasesMoviesApi(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              color: AppColors.whiteColor,
+                            ),
+                          );
+                        } else if (snapshot.hasError || snapshot.data?.statusCode != 200) {
+                          return Column(
+                            children: [
+                              const Text('Something went wrong'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {});
+                                },
+                                child: const Text('Try again'),
+                              ),
+                            ],
+                          );
+                        } else if (snapshot.hasData &&
+                            snapshot.data?.data.results != null &&
+                            snapshot.data!.data.results!.isNotEmpty) {
+                          var newReleases = snapshot.data!.data.results!;
 
-    Positioned(
-    left: width * 0.05,
-    bottom: -height * 0.1,
-    child: Container(
-    width: width * 0.32,
-    height: height * 0.225,
-    decoration: BoxDecoration(
-    color: AppColors.lightGreyColor,
-    borderRadius: BorderRadius.circular(6),
-    ),
-    child: Stack(
-    children: [
-    Center(
-    child: InkWell(
-    onTap: () {
-    Navigator.pushNamed(context, MovieDetailsScreen.routeName);
-    },
-    child: Image.network(
-    imageUrl,
-    fit: BoxFit.fill,
-    width: double.infinity,
-    height: double.infinity,
-    ),
-    ),
-    ),
-    Align(
-    alignment: Alignment.topLeft,
-    child: IconButton(
-    icon: _isTopContainerBookmarked
-    ? const Icon(
-    Icons.bookmark_added,
-      color: AppColors.orangeColor,
-    )
-        : const Icon(
-      Icons.bookmark_add_outlined,
-      color: AppColors.whiteColor,
-    ),
-      onPressed: _handleTopContainerBookmarkChange,
-    ),
-    ),
-    ],
-    ),
-    ),
-    ),
-      Positioned(
-        left: width * 0.4,
-        bottom: height * 0.05,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * 0.02),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                movie.title ?? "Title not available",
-                style: Theme.of(context).textTheme.headlineLarge,
-              ),
-              SizedBox(height: height * 0.005),
-              Text(
-                movie.releaseDate ?? "Date not available",
-                style: Theme.of(context)
-                    .textTheme
-                    .labelLarge!
-                    .copyWith(
-                  color: AppColors.moviesDetailsColor
-                      .withOpacity(0.7),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    ],
-    ),
-    );
-    },
-      options: CarouselOptions(
-        height: height * 0.35,
-        autoPlay: true,
-        aspectRatio: 16 / 9,
-        viewportFraction: 0.8,
-      ),
-    ),
-      SizedBox(height: height * 0.015),
+                          return ContentForNewRelease(
+                            title: releases,
+                            movies: newReleases,
+                            isScrollable: true,
+                            initialBookmarks: _bookmarkedItems,
+                            onBookmarkChanged: _handleContentBookmarkChange,
+                          );
+                        } else {
+                          return Column(
+                            children: [
+                              const Text('No data available'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {}); // Rebuild to retry fetching data
+                                },
+                                child: const Text('Try again'),
+                              ),
+                            ],
+                          );
+                        }
+                      },
+                    ),
 
-      // FutureBuilder for new releases
-      FutureBuilder<HttpResponse<NewReleaseResponse>>(
-        future: ApiManager.getNewReleasesMoviesApi(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(
-                color: AppColors.whiteColor,
-              ),
-            );
-          } else if (snapshot.hasError || snapshot.data?.statusCode != 200) {
-            return Column(
-              children: [
-                const Text('Something went wrong'),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {});
-                  },
-                  child: const Text('Try again'),
-                ),
-              ],
-            );
-          } else if (snapshot.hasData &&
-              snapshot.data?.data.results != null &&
-              snapshot.data!.data.results!.isNotEmpty) {
-            var newReleases = snapshot.data!.data.results!;
+                    // FutureBuilder for recommended content
+                    FutureBuilder<HttpResponse<TopRatedResponse>>(
+                      future: ApiManager.getTopRatedMoviesApi(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              color: AppColors.whiteColor,
+                            ),
+                          );
+                        } else if (snapshot.hasError || snapshot.data?.statusCode != 200) {
+                          return Column(
+                            children: [
+                              const Text('Something went wrong'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {});
+                                },
+                                child: const Text('Try again'),
+                              ),
+                            ],
+                          );
+                        } else if (snapshot.hasData &&
+                            snapshot.data?.data.results != null &&
+                            snapshot.data!.data.results!.isNotEmpty) {
+                          var topRatedMovies = snapshot.data!.data.results!;
 
-            return ContentForNewRelease(
-              title: releases,
-              movies: newReleases,
-              isScrollable: true,
-              initialBookmarks: _bookmarkedItems,
-              onBookmarkChanged: _handleContentBookmarkChange,
-            );
-          } else {
-            return Column(
-              children: [
-                const Text('No data available'),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {}); // Rebuild to retry fetching data
-                  },
-                  child: const Text('Try again'),
-                ),
-              ],
-            );
-          }
-        },
-      ),
-
-      ContentRecommended(
-        title: recommended,
-        itemCount: 5,
-        isScrollable: true,
-        initialBookmarks: _bookmarkedItems,
-        onBookmarkChanged: _handleContentBookmarkChange,
-      ),
-    ],
-    );
-    } else {
-      return Column(
-        children: [
-          const Text('No data available'),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {}); // Rebuild to retry fetching data
+                          return ContentRecommended(
+                            title: recommended,
+                            movies: topRatedMovies,
+                            isScrollable: true,
+                            initialBookmarks: _bookmarkedItems,
+                            onBookmarkChanged: _handleContentBookmarkChange,
+                          );
+                        } else {
+                          return Column(
+                            children: [
+                              const Text('No data available'),
+                              ElevatedButton(
+                                onPressed: () {
+                                  setState(() {}); // Rebuild to retry fetching data
+                                },
+                                child: const Text('Try again'),
+                              ),
+                            ],
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                );
+              } else {
+                return Column(
+                  children: [
+                    const Text('No data available'),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {}); // Rebuild to retry fetching data
+                      },
+                      child: const Text('Try again'),
+                    ),
+                  ],
+                );
+              }
             },
-            child: const Text('Try again'),
           ),
         ],
-      );
-    }
-    },
-        ),
-        ],
-        ),
+      ),
     );
   }
 }
